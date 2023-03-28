83a32f1d345ad6a1b06bde812d3660b3b6255a70
UltiiimateDog <116751881+UltiiimateDog@users.noreply.github.com>
2023-03-28 12:30:07 -0600

    Add files via upload

---
+++ Examen3.java
@@ -0,0 +1,62 @@
+package parcial_3;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+public class Examen3 {
+    private File ListaCSV = new File("C:\\Users\\Alan\\Documents\\UDLAP\\2 Semestre\\POO\\parcial_3\\archive", "emails.csv");
+
+    public void ejecutar()  {
+        String currentLine;
+        File data = new File("C:\\Users\\Alan\\Documents\\UDLAP\\2 Semestre\\POO\\parcial_3\\archive", "data.txt");
+        int[] values = new int[3000];
+        for(int i = 0; i < 3000; i++)   {
+            values[i] = 0;
+        }
+        int j = 0;
+        if (this.ListaCSV.exists())    {
+            try (FileReader fileReader = new FileReader(ListaCSV)) {
+                BufferedReader bufReader = new BufferedReader(fileReader);
+                currentLine = bufReader.readLine();//Esta linea es la que tiene el encabezado de la tabla
+                
+                while( (currentLine = bufReader.readLine()) != null)   {
+                    String[] arregloDeLinea = currentLine.split(",");
+                    if (j > 730 && j < 780)    {
+                        for(int i = 1; i < 3001; i++)   {
+                            values[i-1] += Integer.parseInt(arregloDeLinea[i]);
+                        }
+                    }
+                    if (j > 780)    {
+                        break;
+                    }
+                    j++;
+                }
+                fileReader.close();
+                bufReader.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            
+            if (data.exists() == false)    {
+                try {
+                    data.createNewFile();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            try (PrintWriter writer = new PrintWriter("C:\\Users\\Alan\\Documents\\UDLAP\\2 Semestre\\POO\\parcial_3\\archive\\" + "data.txt")) {
+                for(int i = 0; i < 3000; i++)   {
+                    writer.println(values[i]);
+                }
+                writer.close();
+            } catch (FileNotFoundException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+    }
+}
---
+++ MainEX3.java
@@ -0,0 +1,8 @@
+package parcial_3;
+
+public class MainEX3 {
+    public static void main(String[] args) {
+        Examen3 examen = new Examen3();
+        examen.ejecutar();
+    }
+}
